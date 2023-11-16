2e2fa2105dbcf659e16ebe55a77ee0fb8ebac79d
gerrymoeis <144098317+gerrymoeis@users.noreply.github.com>
2023-11-16 11:51:45 +0700

    Create tugas_alpro2.py
    Gerry Moeis - 23091397164

---
+++ tugas_alpro2.py
@@ -0,0 +1,11 @@
+"""
+    Kelas: 2023E
+    Prodi: D4 Manajemen Informatika
+    Kelompok: 1
+
+    Nama Anggota:
+    1. Gerry Moeis M.D.P - 23091397164
+    2. M. Faiz Noer Rizky - 23091397147
+    3. Dea Ayu Novita Putri - 23091397173
+"""
+
